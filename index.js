22e14ed6f926945dcc13e5128aa26e2768d3a346
Leo Balter <leonardo.balter@gmail.com>
2017-10-18 17:56:49 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const fs = require('fs');
+const yaml = require('js-yaml');
+const { run, resultInterface } = require('./integration.js');
+
+const skipList = yaml.safeLoad(fs.readFileSync(path.join(__dirname, './filters.yml'), 'utf8'));
+
+let count = 0;
+function execute(testObject) {
+  count++;
+  // TODO: execute the test for real
+  return Object.assign({}, resultInterface, {
+    pass: true, // if pass
+    // message: '', // if any
+  })
+}
+
+run(skipList, execute).then(results => {
+  console.log(`Done! Ran ${count} tests out of ${results.length}`);
+});
---
+++ integration.js
@@ -0,0 +1,64 @@
+const path = require('path');
+const testDir = path.join(__dirname, '..', 'test262');
+const TestStream = require('test262-stream');
+
+async function check(testObject, filters, execute) {
+  testObject.file = path.relative('test', testObject.file);
+  testObject.skip = false;
+  testObject.result = {};
+
+  if (filter(testObject, filters)) {
+    // Ready to execute
+    testObject.result = execute(testObject);
+  } else {
+    testObject.skip = true;
+  }
+
+  return testObject;
+}
+
+function filter({file, attrs}, filters) {
+  // Filter tests by location
+  if (filters.location.some(p => file.includes(p))) {
+    return false;
+  }
+
+  // Filter tests by string type tags
+  if (['esid', 'es6id', 'es5id'].some(tag => filters[tag] && filters[tag].includes(attrs[tag]))) {
+    return false;
+  }
+
+  // Filter tests by tagged features
+  if (filters.features &&
+      attrs.features &&
+      filters.features.some(feature => attrs.features.includes(feature))) {
+    return false;
+  }
+
+  // Filter tests by flags (module, async, etc)
+  if (attrs.flags && filters.flags.some(flag => attrs.flags[flag])) {
+    return false;
+  }
+
+  return true;
+}
+
+async function run(filters, execute) {
+  const results = [];
+  const stream = new TestStream(testDir);
+
+  stream.on('data', async testObject => {
+    results.push(await check(testObject, filters, execute));
+  });
+
+  return new Promise((resolve, reject) => {
+     stream.on('error', reject);
+     stream.on('end', () => resolve(results) );
+  });
+}
+
+module.exports.run = run;
+module.exports.resultInterface = {
+  pass: false,
+  message: ''
+};
