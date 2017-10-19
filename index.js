387e2bd0cf3845bc05286e1fc295030b1e74cc9f
Leo Balter <leonardo.balter@gmail.com>
2017-10-19 17:42:50 -0400

    parse code with Prepack

--- index.js
+++ index.js
@@ -2,13 +2,25 @@ const path = require('path');
 const fs = require('fs');
 const yaml = require('js-yaml');
 const { run, resultInterface } = require('./integration.js');
+const Prepack = require('prepack');
 
 const testDir = path.join(__dirname, '..', 'test262');
 const skipList = yaml.safeLoad(fs.readFileSync(path.join(__dirname, './filters.yml'), 'utf8'));
 
-let lastDirName = '';
 function execute(testObject) {
   // TODO: load host and execute the test for real
+
+  let idk;
+
+  try {
+    console.log(testObject.contents);
+    console.log('------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------')
+    idk = Prepack.prepack(testObject.contents);
+    console.log('a', idk.code);
+  } catch(e) {
+    //console.log(e.message);
+  }
+
   return Object.assign({}, resultInterface, {
     pass: true, // if pass
     // message: '', // if any
