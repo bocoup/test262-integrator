2ef0348be92de122fefe277e3384d784f784ca03
Leo Balter <leonardo.balter@gmail.com>
2017-10-31 11:47:47 -0400

    cleanup

--- test/test.js
+++ test/test.js
@@ -7,23 +7,16 @@ const { createAgent } = require('eshost');
 const testDir = path.join(__dirname, '../..', 'test262');
 const filters = yaml.safeLoad(fs.readFileSync(path.join(__dirname, './filters.yml'), 'utf8'));
 
-
-Promise.all([
-  createAgent('node', {
-    hostPath: '/usr/local/bin/node' ,
-    shortName: '$262',
-  })
-]).then(([agent]) => {
-  async function execute({contents, attrs: { flags: { async }}, file}) {
+createAgent('node', {
+  hostPath: '/usr/local/bin/node',
+  shortName: '$262',
+}).then(agent => {
+  async function execute({contents, attrs: { flags: { async }}}) {
     let pass = false;
     let message = '';
-  
+
     const result = await agent.evalScript(contents, {async});
-  
-    // TODO: is this supposed to be an error?
-    if (result.error) {
-      // ...
-    }
+
     return result;
   }
   return Integrator({
@@ -32,16 +25,6 @@ Promise.all([
     execute,
     paths: ['test/language/expressions/class']
   })
-}).then(tests => {
-  const total = results.length;
-  const skipped = results.reduce(
-    ((count, {skip}) => skip ? count++ : count),
-    0
-  );
-
-  const err = results.reduce(
-    ((count, {result: {pass}}) => pass ? count : count++),
-    0
-  );
-  return console.log(`Skipped: ${skipped}, Error: ${err}, Total: ${total}\n`);
-});
+}, err => console.error(err)).then(tests => {
+  return console.log(`Done`);
+}, err => console.error(err));
